091bea695b8a1b78cd494a3760cd73b47651d3e9
EimantasVinciunas <78846024+EimantasV@users.noreply.github.com>
2021-05-28 09:07:57 +0300

    Update Vector.h

--- Vector.h
+++ Vector.h
@@ -3,9 +3,6 @@ template <typename T> class Vector
 
 public:
     typedef T *iterator;
-    // Default constructor to initialise
-    // an initial _capacity of 1 element and
-    // allocating storage using dynamic allocation
     Vector()
     {
         arr = new T[1];
@@ -31,29 +28,20 @@ public:
 
     iterator erase(iterator first, iterator last);
 
-    // Function to add an element at the last
     void push_back(T data)
     {
- 
-        // if the number of elements is equal to the
-        // _capacity, that means we don't have space to
-        // accommodate more elements. We need to double the
-        // _capacity
         if (current == _capacity) {
             T* temp = new T[2 * _capacity];
  
-            // copying old array elements to new array
             for (int i = 0; i < _capacity; i++) {
                 temp[i] = arr[i];
             }
- 
-            // deleting previous array
+
             delete[] arr;
             _capacity *= 2;
             arr = temp;
         }
  
-        // Inserting data
         arr[current] = data;
         current++;
         _first = arr;
@@ -73,13 +61,11 @@ public:
         if(newSize>=_capacity)
         {
             T* temp = new T[newSize];
- 
-            // copying old array elements to new array
+
             for (int i = 0; i < current; i++) {
                 temp[i] = arr[i];
             }
- 
-            // deleting previous array
+
             delete[] arr;
             arr = temp;
         }
@@ -88,20 +74,15 @@ public:
         current = newSize;
     }
  
-    // function to extract element at any index
     T get(int index)
     {
- 
-        // if index is within the range
         if (index < current)
             return arr[index];
     }
  
- 
-    // function to get size of the vector
+
     int size() { return current; }
  
-    // function to get _capacity of the vector
     int capacity() { return _capacity; }
  
 private:
